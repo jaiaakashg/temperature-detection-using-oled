86f73a9f53ddfbac4ddbd029844cd6ccf8304176
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-08 15:47:00 +0530

    Add files via upload

---
+++ tempereature_detection_using_oled.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+
+#define OLED_RESET    -1 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);    
+  display.setTextColor(SSD1306_WHITE); 
+  display.setCursor(0, 10);     
+  display.println(F("Initializing..."));
+  display.display();
+  delay(2000); 
+}
+
+void loop() {
+  display.clearDisplay();
+  float temperature = 25.0 + random(-50, 50) / 10.0; 
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+  display.print("Temp: ");
+  display.print(temperature);
+  display.println(" C");
+
+  display.display();
+  delay(1000); 
+}
+
